935eb299a1e1dc1577bddf3e2536975e3e3c7c94
汪梓文 <wangziwen@wangziwendeMacBook-Pro.local>
2017-09-15 13:35:44 +0800

    s

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ms-fruits
-ms-fruits
+ms-
--- src/main/java/wowjoy/fruits/ms/config/WebConfig.java
+++ src/main/java/wowjoy/fruits/ms/config/WebConfig.java
@@ -3,17 +3,24 @@ package wowjoy.fruits.ms.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import wowjoy.fruits.ms.util.ArgumentInterceptor;
 
 import java.util.List;
 
 /**
- * Created by wangziwen on 2017/9/15.
+ * Created by wangziwen on 2017/8/30.
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport{
+public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
-    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new ArgumentInterceptor());
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new MappingJackson2HttpMessageConverter());
     }
 }
