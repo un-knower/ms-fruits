149e11fc22ad34677ea711863a99db53355955ee
汪梓文 <wangziwen@wangziwendeMacBook-Pro.local>
2017-11-06 14:57:48 +0800

    s

--- src/main/java/wowjoy/fruits/ms/controller/UserController.java
+++ src/main/java/wowjoy/fruits/ms/controller/UserController.java
@@ -3,12 +3,14 @@ package wowjoy.fruits.ms.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import wowjoy.fruits.ms.dao.user.AbstractDaoAccount;
 import wowjoy.fruits.ms.dao.user.AbstractDaoUser;
 import wowjoy.fruits.ms.dao.user.AccountDaoImpl;
 import wowjoy.fruits.ms.dao.user.UserDaoImpl;
 import wowjoy.fruits.ms.module.user.FruitAccountVo;
 import wowjoy.fruits.ms.module.user.FruitUserVo;
+import wowjoy.fruits.ms.util.ApplicationContextUtils;
 import wowjoy.fruits.ms.util.JsonArgument;
 import wowjoy.fruits.ms.util.RestResult;
 
@@ -19,19 +21,16 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/v1/api/user")
 public class UserController {
-    @Resource(type = UserDaoImpl.class)
-    private AbstractDaoUser dataUser;
     @Resource(type = AccountDaoImpl.class)
     private AbstractDaoAccount daoAccount;
 
     @RequestMapping(method = RequestMethod.GET)
-    public RestResult finds(@JsonArgument(type = FruitUserVo.class) FruitUserVo vo) {
-        return RestResult.getInstance().setData(dataUser.finds(vo));
+    public RestResult finds() {
+        return RestResult.getInstance().setData(ApplicationContextUtils.getCurrentUser());
     }
 
     @RequestMapping(value = "/account", method = RequestMethod.GET)
     public RestResult account(@JsonArgument(type = FruitAccountVo.class) FruitAccountVo vo) {
         return RestResult.getInstance().setData(daoAccount.findByAccount(vo));
     }
-
 }
