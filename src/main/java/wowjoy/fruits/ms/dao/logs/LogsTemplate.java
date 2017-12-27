7c9fb73c037607a6f5ba2904669e37c6d0eabcec
汪梓文 <wangziwen@wangziwendeMacBook-Pro.local>
2017-12-27 15:44:06 +0800

    s

--- src/main/java/wowjoy/fruits/ms/dao/logs/LogsTemplate.java
+++ src/main/java/wowjoy/fruits/ms/dao/logs/LogsTemplate.java
@@ -125,7 +125,8 @@ public abstract class LogsTemplate<T extends AbstractEntity> {
         public T fromJson(FruitLogsDao log) {
             T plan = new Gson().fromJson(new JsonParser().parse(log.getJsonObject()), TypeToken.of(FruitPlanDao.class).getType());
             plan.computeDays();
-            log.setOperateType(FruitDict.LogsDict.valueOf(plan.getPlanStatus()));
+            if (plan.getDays() < 0)
+                log.setOperateType(FruitDict.LogsDict.valueOf(plan.getPlanStatus()));
             return plan;
         }
     }
